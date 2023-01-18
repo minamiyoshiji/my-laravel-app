24ffbe2ab220f4d1920eca3cf77aaaa518609659
minami <yoshiji0420@icloud.com>
2023-01-18 15:35:53 +0900

    update README

--- app/resources/views/tasks/index.blade.php
+++ app/resources/views/tasks/index.blade.php
@@ -43,7 +43,7 @@
 <h1>タスク一覧</h1>
 <div class="container">
   <div class="task__add">
-      <a href="{{ route('tasks.add') }}">＋仕事を追加する</a>
+      <a href="{{ route('tasks.add') }}">＋タスクを追加する</a>
   </div>
     <table>
         <tr>
