2dfc0ad8b4da8c03e44f93a6b78da63f4f9d9df8
minami <yoshiji0420@icloud.com>
2023-01-22 02:50:08 +0900

    4回目

--- app/app/Http/Controllers/Auth/RegisterController.php
+++ app/app/Http/Controllers/Auth/RegisterController.php
@@ -29,7 +29,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/folders/{id}/tasks';
 
     /**
      * Create a new controller instance.
