ed44c34a92590166f2713af2408e1a5b2e01c99f
minami <yoshiji0420@icloud.com>
2023-01-22 03:16:15 +0900

    4回目

--- app/app/Http/Controllers/Auth/LoginController.php
+++ app/app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/folders/{id}/tasks';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
